68c1d7ae099567956eea92fe9d1b46c21cbf0d24
Priscila Andreani <priscila.andreani@grupoboticario.com.br>
2025-11-25 17:09:38 -0300

    refactor: rename async methods

--- services/google/async-stt.js
+++ services/google/async-stt.js
@@ -16,13 +16,17 @@ export async function speechToTextWithGoogle(gcsUri) {
       },
     };
 
-    const [response] = await sttClient.recognize(request);
+    const [operation] = await sttClient.longRunningRecognize(request);
+    console.log("Aguardando o resultado da operação...");
+
+    const [response] = await operation.promise();
+
     const transcription = response.results
       .map((result) => result.alternatives[0].transcript)
       .join("\n");
 
-    console.log(`Transcrição: ${transcription}`);
-    return transcription;
+    console.log(`\nTranscrição concluída:`);
+    console.log(transcription);
   } catch (error) {
     console.error("Erro ao transcrever áudio com Google STT:", error);
     throw error;
